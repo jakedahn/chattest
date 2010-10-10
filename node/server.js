0afa5f462e141d4c3a00051391963571b30a6ecc
Jake Dahn <admin@jakedahn.com>
2010-10-09 23:44:59 -0500

    now allows global queries

--- node/server.js
+++ node/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': "http://looce.com"});
+  res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': "*"});
   res.end('Hello World\n');
   console.log("request done");
 }).listen(3333, "0.0.0.0");
